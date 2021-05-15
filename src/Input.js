c6d12af10c73e1adc8ba2f6f911aa6a6b8b99e3a
Mikael Scientia <mikaelmuniz2001@gmail.com>
2021-05-14 21:23:46 -0300

    axios api

---
+++ src/Input.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Input extends React.Component {
+    state = {
+        name: '',
+    };
+    handleChange = event => {
+        this.setState({name: event.target.value });
+    };
+    handleSubmit = event => {
+        event.preventDefault();
+
+        const user = {
+            name: this.state.name,
+        }
+
+        axios
+        .post('http://jsonplaceholder.typicode.com/users', { user })
+        .then(res => {
+            console.log(res)
+            console.log(res.data);
+        });
+    };
+    render() {
+        return ( 
+            <form>
+                <label onSubmit={this.handleSubmit}>
+                Person Name: 
+                <input type="text" name="name" onChange={this.handleChange} />
+                <button type="submit">
+                Add
+                </button>
+                </label>
+            </form>
+        );
+    }
+}
